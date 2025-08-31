96ce975ccc33bb37524b692d2b5dabc6da683221
rachel.wu <119239275@qq.com>
2025-08-31 09:14:51 +0800

    change helloworld

--- ch1_1/HelloWorld.java
+++ ch1_1/HelloWorld.java
@@ -5,9 +5,8 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
     }
-
-
+    
     // terminal command to run
     // javac HelloWorld.java
-    // java -classpath /Users/wuyuan/src/csa/out/production/csa ch1_1.HelloWorld
+    // java -classpath /Users/wuyuan/src/csa/ ch1_1.HelloWorld
 }
