01236826ccac0153bbdbacf1c96377a62e77df4c
rachel.wu <119239275@qq.com>
2025-09-08 20:03:37 +0800

    csa

--- ch1_1/HelloWorld.java
+++ ch1_1/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 
     //Print greeting messages to the names that are entered in stdIn
     public static void main(String[] args){
+        //System.out.println("Hello World");
 
+        //example for runtime error
         System.out.println("Hello  " + args[0] + " and " +args[1] );
     }
 
