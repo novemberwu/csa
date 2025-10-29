124ab5954f19d0528a3f0cc7e397f0978c67406f
rachel.wu <119239275@qq.com>
2025-10-29 09:33:20 +0800

    array copy example

---
+++ ch4_2array_copy/PQArray.java
@@ -0,0 +1,27 @@
+package ch4_2array_copy;
+
+public class PQArray {
+    public void main(String[] args){
+        int[] a = new int[6];
+        int[] b = new int[a.length];
+
+        b = a;
+        for(int i =0; i < b.length; i++){
+            b[i] = i;
+        }
+
+
+        // what will be values in array a?
+        for(int i = 0; i < a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+
+
+        // what will be values in array b?
+        for(int i = 0; i < b.length; i++){
+            System.out.print(b[i] + " ");
+        }
+        System.out.println();
+    }
+}
