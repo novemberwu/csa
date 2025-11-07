8c00a6ec9686e3d8876eec657e3b6b4d76587b9f
rachel.wu <119239275@qq.com>
2025-11-07 09:36:05 +0800

    adding store

--- midTerm/Store.java
+++ midTerm/Store.java
@@ -1,14 +1,31 @@
 package midTerm;
 
 public class Store {
-
-    public void setName(String name){}
-
-    public static double calculateBill(int bill){
-        return (double) bill;
+    private static int count = 0;
+    static {
+        System.out.println("There are " + count + " stores.");
     }
-
+    public static int getCount(){
+        return count;
+    }
+    private String storeName;
+    public Store(){
+        this.storeName = "Default name";
+    }
+    public Store(String name){
+        this.storeName = name;
+    }
+    public String getName(){
+        return this.storeName;
+    }
+    public void setName(String name){
+        this.storeName = name;
+    }
+    public static double calculateBill(int amount, double price) {
+        return price * amount;
+    }
     public static void main(String[] args){
-        //TODO call calculate Bill
+        System.out.println("Inside main");
+        System.out.println(Store.getCount() + " stores");
     }
 }
--- midTerm/StoreClient.java
+++ midTerm/StoreClient.java
@@ -1,16 +1,14 @@
 package midTerm;
 
 public class StoreClient {
-    static{
-        System.out.println("Store client is loading");
-    }
+
     public static void main(String[] args){
         // TODO call calculateBill of Store class
         // TODO call setName of Store class
         System.out.println("inside main");
 
 
-      
+
 
 
     }
